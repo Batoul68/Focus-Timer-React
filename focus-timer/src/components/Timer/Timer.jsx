e2bbd39313a1e9801a3628a7bbf9373b2edf02e3
Batoul <batoulalso68@gmail.com>
2026-07-24 17:03:18 -0400

    basic template before functionality added

--- focus-timer/src/components/Timer/Timer.jsx
+++ focus-timer/src/components/Timer/Timer.jsx
@@ -1,5 +1,6 @@
 import frame1 from '../../assets/frame.svg'
 import cat1 from '../../assets/cat_asleep.gif'
+import cat2 from '../../assets/cat_awake.gif'
 import styles from './Timer.module.css'
 import TimerControls from "../TimerControls/TimerControls.jsx"
 
@@ -9,11 +10,12 @@ function Timer() {
     <div className={styles.timer}>
       <img className={styles.timerCat} src={cat1} alt="asleep cat"></img>
       <img className={styles.timerFrame} src={frame1} alt="timer frame"></img>
+      <div className={styles.timerNumber}>25:00</div>
       <TimerControls />
     </div>
 
   );
 }/*
       
-<div className={styles.timerNumber}>25.00</div>      */
+      */
 export default Timer
