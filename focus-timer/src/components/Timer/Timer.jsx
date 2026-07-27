3d7190e6880b5d059029a8d7dcf6f6269028e229
Batoul <batoulalso68@gmail.com>
2026-07-27 12:24:54 -0400

    cat changes based on if timer is running

--- focus-timer/src/components/Timer/Timer.jsx
+++ focus-timer/src/components/Timer/Timer.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import frame1 from '../../assets/frame.svg';
-import cat1 from '../../assets/cat_asleep.gif';
-import cat2 from '../../assets/cat_awake.gif';
+import catAsleep from '../../assets/cat_asleep.gif';
+import catAwake from '../../assets/cat_awake.gif';
 import styles from './Timer.module.css';
 import TimerControls from "../TimerControls/TimerControls.jsx";
 const STARTING_TIME = 25*60;
@@ -52,7 +52,7 @@ function Timer() {
 
   return(
     <div className={styles.timer}>
-      <img className={styles.timerCat} src={cat1} alt="asleep cat"></img>
+      <img className={styles.timerCat} src={isRunning ? catAwake : catAsleep} alt="asleep cat"></img>
       <img className={styles.timerFrame} src={frame1} alt="timer frame"></img>
       <div className={styles.timerNumber}>
         <p>
