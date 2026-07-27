ac6352f951cabe0d3d27e4ce27013b5b1d328776
Batoul <batoulalso68@gmail.com>
2026-07-27 12:15:38 -0400

    added buttons, added start functionality

--- focus-timer/src/components/Timer/Timer.jsx
+++ focus-timer/src/components/Timer/Timer.jsx
@@ -1,21 +1,59 @@
-import frame1 from '../../assets/frame.svg'
-import cat1 from '../../assets/cat_asleep.gif'
-import cat2 from '../../assets/cat_awake.gif'
-import styles from './Timer.module.css'
-import TimerControls from "../TimerControls/TimerControls.jsx"
+import { useEffect, useState } from 'react';
+import frame1 from '../../assets/frame.svg';
+import cat1 from '../../assets/cat_asleep.gif';
+import cat2 from '../../assets/cat_awake.gif';
+import styles from './Timer.module.css';
+import TimerControls from "../TimerControls/TimerControls.jsx";
 
 function Timer() {
+
+  const [timeLeft, setTimeLeft] = useState(25*60);
+  
+  const minutes = Math.floor(timeLeft/60);
+  const seconds = timeLeft % 60;
+
+  const [isRunning, setIsRunning] = useState(false);
+
+  function handleStart() {
+    setIsRunning(true);
+  }
+
+  useEffect(() => {
+    if (!isRunning) {
+      return;
+    }
+
+    const intervalId = setInterval(() => {
+      setTimeLeft((currentTime) => {
+        if (currentTime <= 1) {
+          setIsRunning(false);
+          return 0;
+        }
+        
+        return currentTime - 1;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+
+  }, [isRunning]);
 
   return(
     <div className={styles.timer}>
       <img className={styles.timerCat} src={cat1} alt="asleep cat"></img>
       <img className={styles.timerFrame} src={frame1} alt="timer frame"></img>
-      <div className={styles.timerNumber}>25:00</div>
-      <TimerControls />
+      <div className={styles.timerNumber}>
+        <p>
+          {String(minutes).padStart(2, "0")}:
+          {String(seconds).padStart(2, "0")}
+        </p>
+      </div>
+      <TimerControls onStart={handleStart}/>
     </div>
 
   );
-}/*
-      
-      */
+}
+
 export default Timer
--- focus-timer/src/components/TimerControls/TimerControls.jsx
+++ focus-timer/src/components/TimerControls/TimerControls.jsx
@@ -1,16 +1,28 @@
-import styles from './TimerControls.module.css'
-import startButton from '../../assets/startButton.svg'
-import pauseButton from '../../assets/pauseButton.svg'
-import resetButton from '../../assets/resetButton.svg'
+import styles from './TimerControls.module.css';
+import startButton from '../../assets/startButton.svg';
+import pauseButton from '../../assets/pauseButton.svg';
+import resetButton from '../../assets/resetButton.svg';
 
-function TimerControls() {
-
+function TimerControls({onStart}) {
   return(
     <div className={styles.timerControls}>
-      <img className={styles.startButton} src={startButton} alt="start button"></img>
-      <img className={styles.pauseButton} src={pauseButton} alt="pause button"></img>
-      <img className={styles.resetButton} src={resetButton} alt="reset button"></img>
+      <button 
+        className={styles.buttonWrapper}
+        onClick={onStart}
+        aria-label="Start timer"
+      >
+        <img className={styles.startButton} src={startButton} alt="start button"></img>
+      </button>
 
+      <button className={styles.buttonWrapper}>
+        <img className={styles.pauseButton} src={pauseButton} alt="pause button"></img>
+      </button>
+
+      <button className={styles.buttonWrapper}>
+        <img className={styles.resetButton} src={resetButton} alt="reset button"></img>
+      </button>
+      
+      
     </div>
   );
 }
